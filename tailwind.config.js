8976d0e9ee2d8bd04ec83a0545d88ab35c02af26
Victor Perez <zunaty@gmail.com>
2021-12-14 11:56:41 -0700

    Choosing colors and initial tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
     "./pages/index.js"
   ],
   theme: {
+    container: {
+      center: true
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'blush': '#EA638C',
+      'pale': '#FFD9DA',
+      'claret': '#89023E',
+      'gunmetal': '#30343F',
+      'black': '#1B2021'
+    },
     extend: {},
   },
   plugins: [],
